38b393753eefefe4462f84aaf718f4052f9f58a0
JosephJosue <josephjosue11@gmail.com>
2025-07-17 07:08:46 -0500

    cambio de puerto en app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, redirect, url_for, session
 
 app = Flask(__name__)
@@ -33,4 +34,6 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
